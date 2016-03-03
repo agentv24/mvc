d8f82275f95d13b7e55fa00c0095ca9f7b9f9180
agentv24 <winston.huey@gmail.com>
2016-03-03 10:09:40 -0500

    changed view

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-	<title>Test</title>
+	<title>Starting New Deployment</title>
 	<link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css" />
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
