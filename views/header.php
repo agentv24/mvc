7db5fe9b36e40c7e5dad8639deeb82f9b949aeef
agentv24 <winston.huey@gmail.com>
2016-03-03 10:11:55 -0500

    just added Jan line

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-	<title>Starting New Deployment</title>
+	<title>Starting New Deploymen for Jan</title>
 	<link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css" />
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
